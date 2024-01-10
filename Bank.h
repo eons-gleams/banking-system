92f03ca1dc1a7cee94e26b64b46716eb134cfd91
David Sutton <153441616+eons-gleams@users.noreply.github.com>
2024-01-10 14:13:51 +0700

    Create Bank.h

---
+++ Bank.h
@@ -0,0 +1,16 @@
+#ifndef BANK_H
+#define BANK_H
+
+#include <vector>
+#include "Customer.h"
+
+class Bank {
+public:
+    void addCustomer(const Customer& customer);
+    void processTransaction(const Transaction& transaction);
+
+private:
+    std::vector<Customer> customers;
+};
+
+#endif // BANK_H
